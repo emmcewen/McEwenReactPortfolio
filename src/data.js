9a2ba49a0d2166ff9229826b4b9081d1f2b9acb6
EMILY MCEWEN <emmcewen@gmail.com>
2022-10-13 19:45:34 -0400

    adding data.js for projects

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "Flicks Movie Generator",
+      subtitle: "Javascript & Restful API",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "",
+    },
+    {
+      title: "Storylines",
+      subtitle: "Handlebars, Express.js, Node.js",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "",
+    },
+    {
+      title: "Weather Dashboard",
+      subtitle: "Jquery, Javascript & Restful API",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "",
+    },
+    {
+      title: "Ecommerce Backend",
+      subtitle: "Node.js, MySQL, Express.js, Sequelize, Insomnia",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "",
+    },
+  ];
